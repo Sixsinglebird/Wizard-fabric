4d3abfd56c8cd5164bda0dc130015c70903a8edd
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-10-12 00:34:11 -0230

    ADding wand assets and initial framework

---
+++ src/client/java/com/wizardfabric/WizardFabricClient.java
@@ -0,0 +1,10 @@
+package com.wizardfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WizardFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/wizardfabric/Wand.java
@@ -0,0 +1,10 @@
+package com.wizardfabric;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+
+public class Wand extends Item {
+    public Wand(Settings settings) {
+        super(settings);
+    }
+}
